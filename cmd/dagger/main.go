a56bd9b2f7b800e2408b29542a27792147e1cdd8
Erik Sipsma <erik@sipsma.dev>
2022-07-06 10:55:17 -0700

    add pprof endpoint for temp debugging
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- cmd/dagger/main.go
+++ cmd/dagger/main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
 	"go.dagger.io/dagger/cmd/dagger/cmd"
 )
+
+func init() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+}
 
 func main() {
 	cmd.Execute()
