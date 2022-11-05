3566a6c2bb50a06acc5f9e0458b5a17896a38ca9
Erik Sipsma <erik@sipsma.dev>
2022-11-05 16:42:41 -0700

    Add initial prototype of python SDK provisioner.
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- sdk/python/src/dagger/connectors/http.py
+++ sdk/python/src/dagger/connectors/http.py
@@ -1,7 +1,12 @@
+import hashlib
 import logging
+import os
+from pathlib import Path
 import sys
+import tempfile
 from asyncio.subprocess import Process
-from subprocess import DEVNULL, CalledProcessError
+from subprocess import DEVNULL, CalledProcessError, PIPE, STDOUT
+from urllib.parse import urlparse
 
 import anyio
 from aiohttp import ClientTimeout
@@ -16,6 +21,12 @@ from .base import Config, Connector, register_connector
 
 logger = logging.getLogger(__name__)
 
+normalized_arch = {
+  "x86_64": "amd64",
+  "amd64": "amd64",
+  "aarch64": "arm64",
+  "arm64": "arm64",
+}
 
 @define
 class Engine:
@@ -23,24 +34,98 @@ class Engine:
 
     _proc: Process | None = field(default=None, init=False)
 
-    async def assert_version(self) -> None:
-        try:
-            await anyio.run_process(["cloak", "dev", "--help"], stdout=DEVNULL, stderr=DEVNULL, check=True)
-        except CalledProcessError:
-            logger.error("⚠️  Please ensure that cloak binary in $PATH is v0.3.0 or newer.")
-            # FIXME: make sure resources are being cleaned up correctly
-            sys.exit(127)
-
     async def start(self) -> None:
-        await self.assert_version()
-        args = ["cloak", "dev"]
+        cache_dir = Path(os.environ.get("XDG_CACHE_HOME", Path.home() / ".cache")) / "dagger"
+        cache_dir.mkdir(parents=True, exist_ok=True, mode=0o700)
+
+        # TODO: this should be a config option obviously
+        image_ref = "docker.io/eriksipsma/test-dagger:rebase@sha256:b6bcf40346a70a834a0c1461046551993b17c92288c32121b171a825958e7901"
+
+        # TODO: not sure where consts usually go
+        digest_len = 16
+
+        # Check to see if image_ref contains @sha256:, if so use the digest as the id.
+        is_pinned = False
+        if "@sha256:" in image_ref:
+          id = image_ref.split("@sha256:", maxsplit=1)[1]
+          # TODO: add verification that the digest is valid (not something malicious with / or ..)
+          is_pinned = True
+        else:
+          # set id to the sha256 hash of the image_ref
+          # TODO: ensure that this is consistent w/ Go's sha256 hash (encoding is only likely source of difference)
+          id = hashlib.sha256(image_ref.encode()).hexdigest()
+        id = id[:digest_len]
+
+        helper_bin_path = cache_dir / ("dagger-sdk-helper-" + id)
+        container_name = "dagger-engine-" + id
+
+        docker_run_args = [
+            "docker", "run",
+            "--name", container_name,
+            "-d",
+            "--restart", "always",
+            "--privileged",
+            image_ref,
+            "--debug",
+        ]
+        docker_run_result = await anyio.run_process(
+            docker_run_args, 
+            stdout=PIPE, 
+            stderr=STDOUT,
+            check=False,
+        )
+        if docker_run_result.returncode != 0:
+          if f"Conflict. The container name \"/{container_name}\" is already in use by container" not in docker_run_result.stdout.decode():
+            raise Exception("Failed to start engine container: " + docker_run_result.stdout.decode())
+
+        # TODO: garbage collection of old containers
+
+        uname = os.uname()
+        self_arch = normalized_arch.get(uname.machine.lower())
+        self_os = uname.sysname.lower()
+
+        if not helper_bin_path.exists():
+          tmp_bin = tempfile.NamedTemporaryFile(prefix='dagger-sdk-helper-', dir=cache_dir, delete=True)
+          docker_cp_args = [
+              "docker", "cp",
+              container_name + f":/usr/bin/dagger-sdk-helper-{self_os}-{self_arch}",
+              tmp_bin.name,
+          ]
+          docker_cp_result = await anyio.run_process(
+              docker_cp_args, 
+              stdout=PIPE, 
+              stderr=STDOUT,
+              check=False,
+          )
+          if docker_cp_result.returncode != 0:
+            raise Exception("Failed to copy helper binary: " + docker_cp_result.stdout.decode())
+          os.chmod(tmp_bin.name, 0o700)
+          if is_pinned:
+            os.rename(tmp_bin.name, helper_bin_path)
+          else:
+            helper_bin_path = Path(tmp_bin.name)
+
+        # TODO: garbage collection of old helper binaries
+
+        buildkit_host = "docker-container://" + container_name
+
+        helper_args = [helper_bin_path, "--remote", buildkit_host]
         if self.cfg.workdir:
-            args.extend(["--workdir", str(self.cfg.workdir.absolute())])
-        if self.cfg.host.port:
-            args.extend(["--port", str(self.cfg.host.port)])
+            helper_args.extend(["--workdir", str(self.cfg.workdir.absolute())])
         if self.cfg.config_path:
-            args.extend(["-p", str(self.cfg.config_path.absolute())])
-        self._proc = await anyio.open_process(args)
+            helper_args.extend(["--project", str(self.cfg.config_path.absolute())])
+
+        # TODO: log output should be configurable, not hardcoded to our stderr
+        stdoutr, stdoutw = os.pipe()
+        stdoutr = os.fdopen(stdoutr)
+        self._proc = await anyio.open_process(helper_args, stdin=PIPE, stdout=stdoutw, stderr=sys.stderr)
+
+        # read port number from first line of stdout
+        port = int(stdoutr.readline().strip())
+        stdoutr.close()
+        # TODO: verify port number is valid
+
+        self.cfg.host = f"http://localhost:{port}"
 
     async def stop(self) -> None:
         assert self._proc is not None
@@ -69,11 +154,9 @@ class HTTPConnector(Connector):
         return await super().connect()
 
     async def provision(self) -> None:
-        # FIXME: only provision if port is not in use
         # FIXME: handle cancellation, retries and timeout
         # FIXME: handle errors during provisioning
-        # await self.engine.start()
-        ...
+        await self.engine.start()
 
     async def close(self) -> None:
         # FIXME: need exit stack?
