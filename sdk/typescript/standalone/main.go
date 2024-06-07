4c85e5047e9c4a1ed71b82a17fe710e53c1b93d8
Erik Sipsma <erik@sipsma.dev>
2024-06-07 10:45:08 -0700

    wip
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

---
+++ sdk/typescript/standalone/main.go
@@ -0,0 +1,20 @@
+package main
+
+type Ts struct{}
+
+func (m *Ts) ModuleRuntime(modSource *ModuleSource, introspectionJSON string) *Container {
+	return modSource.WithSDK("typescript").AsModule().Runtime().WithExec([]string{"npm", "install", "-g", "tsx@4.13.0"}, ContainerWithExecOpts{SkipEntrypoint: true})
+}
+
+// Returns lines that match a pattern in the files of the provided Directory
+func (m *Ts) Codegen(modSource *ModuleSource, introspectionJSON string) *GeneratedCode {
+	return dag.GeneratedCode(modSource.WithSDK("typescript").AsModule().GeneratedContextDirectory())
+}
+
+func (m *Ts) RequiredPaths() []string {
+	return []string{
+		"**/package.json",
+		"**/package-lock.json",
+		"**/tsconfig.json",
+	}
+}
