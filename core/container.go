e2f669d964602326ccbce8676ff676747ea77092
Erik Sipsma <erik@sipsma.dev>
2025-08-26 15:22:45 -0700

    simplify container dagop cache key to just be id digest
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- core/container.go
+++ core/container.go
@@ -215,6 +215,7 @@ func (container *Container) OnRelease(ctx context.Context) error {
 	if container == nil {
 		return nil
 	}
+	// TODO: this might be problematic if directories overlap, could release same result multiple times
 	if rootfs := container.FS; rootfs != nil {
 		rootfsRes := rootfs.Self().Result
 		if rootfsRes != nil {
@@ -234,21 +235,25 @@ func (container *Container) OnRelease(ctx context.Context) error {
 		}
 	}
 	for _, mount := range container.Mounts {
-		if src := mount.DirectorySource.Self(); src != nil {
-			res := src.Result
-			if res != nil {
-				err := res.Release(ctx)
-				if err != nil {
-					return err
+		if src := mount.DirectorySource; src != nil {
+			if src := mount.DirectorySource.Self(); src != nil {
+				res := src.Result
+				if res != nil {
+					err := res.Release(ctx)
+					if err != nil {
+						return err
+					}
 				}
 			}
 		}
-		if src := mount.FileSource.Self(); src != nil {
-			res := src.Result
-			if res != nil {
-				err := res.Release(ctx)
-				if err != nil {
-					return err
+		if src := mount.FileSource; src != nil {
+			if src := mount.FileSource.Self(); src != nil {
+				res := src.Result
+				if res != nil {
+					err := res.Release(ctx)
+					if err != nil {
+						return err
+					}
 				}
 			}
 		}
